25aef41b6f0055196b63fc6d0a59892cd9fa5bb2
armando.duran2 <armando.duran2@bbva.com>
2018-12-30 02:17:58 -0600

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Trips Flutter',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+                leading: Icon(Icons.arrow_back),
+                title: Text('Share')),
+            body: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/background.jpg"),
+                        fit: BoxFit.cover
+                    )
+                ),
+                child:Center(
+                    child:Container(
+                      color: Colors.black26,
+                      height: 60,
+                      child: Center(
+                        child: Text(
+                          "Hello it's my first screen with flutter",
+                          style: TextStyle(
+                            color: Colors.cyanAccent,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    )
+                )
+            )
+        )
+    );
+  }
+}
+
